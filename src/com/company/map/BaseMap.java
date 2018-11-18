d37500553beec97031d7fc55b5d8813a4ff37458
maxim <maksim.schapov87@yandex.ru>
2018-11-18 14:26:13 +0400

    baseMap update

--- src/com/company/map/BaseMap.java
+++ src/com/company/map/BaseMap.java
@@ -12,13 +12,39 @@ enum BaseMapCellType {
 public class BaseMap {
     private BaseMapCellType[][] map;
 
+BaseMap() { //рисовка стен                        
+    map = new BaseMapCellType [10][10];
+    for (int i = 0; i < map.length; i++) {
+        for (int j = 0; j < map[i].length;j++) {
+            if (map[i][9] == BaseMapCellType.WALL){
+
+            }
+        for (int i = 0; i < map.length; j++){
+            for (int j = 0; j < map[i].length;j++){
+            if (map[9][i] == BaseMapCellType.WALL){
+
+                     }
+   
     public BaseMapCellType[][] getMap() {
         return map;
     }
 
     public void setMap(BaseMapCellType[][] map) {
         this.map = map;
-    }
-
+        
+                      }
+     void drawMap (Graphics graphics){
+         for (int i = 0; i< map.length;i++){
+             for (int j = 0; j < map[i].length;j++){
+                 if (map [i][j] == BaseMapCellType.PLAYER){
+                                 }
+                            }
+                        }
+                     }                
+                  }    
+             }
+        }
+    }   
+}
     // TODO: - realize map drawing
 }
